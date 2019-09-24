87960fab92228572f951b8987e0ceeb377397ef9
Leandro Rocha <prog.leandro@gmail.com>
2019-09-23 23:18:25 -0300

    Estudando o React Navigation

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
+console.tron.log(Routes);
+
+export default function App() {
+  return <Routes />;
+}
